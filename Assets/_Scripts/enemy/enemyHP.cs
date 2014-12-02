967d43cd33d08be1c0f1a8b19c9ec50a5ed931cf
Nick van Dokkum <16072@ma-web.nl>
2014-12-02 16:31:26 +0100

    added different bullets, added enemy HP, added Nick.scene

---
+++ Assets/_Scripts/enemy/enemyHP.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyHP : MonoBehaviour {
+
+	private int HP = 100;
+	
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "BulletDamage")
+		{
+			HP -= 50;
+		}
+		else if (other.gameObject.tag == "BulletFast")
+		{
+			HP -= 10;
+		}
+		else if (other.gameObject.tag == "BulletNorm")
+		{
+			HP -= 25;
+		}
+		Destroy (other.gameObject);
+		if(HP < 0) {
+			Destroy (gameObject);
+		}
+	}
+}
