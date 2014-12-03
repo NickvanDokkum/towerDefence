73b5447bfc2eff667b703088872e4fc4376a04c0
Kramboja <koen5067@icloud.com>
2014-12-03 16:50:55 +0100

    UpgradeSystemWorking

--- Assets/_Scripts/WallHost.cs
+++ Assets/_Scripts/WallHost.cs
@@ -11,6 +11,7 @@ public class WallHost : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
+		//Hoe zet je dit object in een leeg game object?
 		Transform trans = (Transform)Instantiate(walls[0], new Vector3(transform.position.x,0.55f,transform.position.z), transform.rotation);
 		trans.parent = transform;
 	}
@@ -30,22 +31,32 @@ public class WallHost : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		Debug.Log (_OnHover);
-
 		//kijkt of je een object kan upgraden
 		if (Input.GetKeyUp (KeyCode.B)) 
 		{
+			Debug.Log("test1");
 			if (_OnHover)
 			{
+				Debug.Log("test2");
 				if(currentWallLevel < walls.Length)
 				{
-					if (GameObject.Find ("Floor").GetComponent<FloorManager> ().BuildMode) 
+					removeOldChild();
+
+					if(GameObject.Find("Floor").GetComponent<FloorManager>().BuildMode)
 					{
 						currentWallLevel++;
-						var go = Instantiate(walls[currentWallLevel], new Vector3(transform.position.x,0.55f,transform.position.z), transform.rotation);
+						Transform trans = (Transform)Instantiate(walls[currentWallLevel], new Vector3(transform.position.x,10f,transform.position.z), transform.rotation);
+						trans.parent = transform;
 					}
 				}
 			}
 		}
+	}
+	void removeOldChild()
+	{
+		Transform trans = this.transform.GetChild(currentWallLevel);
+		trans.transform.parent = null;
+
+		Destroy ((trans as Transform).gameObject);
 	}
 }
