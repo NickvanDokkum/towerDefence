e7911f7063a3fa90f5f9376555dbaa01f5a1c361
NickvanDokkum <16072@ma-web.nl>
2015-01-14 14:03:40 +0100

    pushed turret 1

--- Assets/_Scripts/enemy/enemyHP.cs
+++ Assets/_Scripts/enemy/enemyHP.cs
@@ -26,10 +26,11 @@ public class enemyHP : MonoBehaviour {
 		}
 		if(HP <= 0) {
 			Globals.Score += 9 + Globals.waveNumber;
-			int coinsTotal = Random.Range(1,3);
+			int coinsTotal = Random.Range(0,4);
+			print(coinsTotal);
 			for(int i = 0; i < coinsTotal; i++)
 			{
-				Instantiate(coin, new Vector3(transform.position.x + Random.Range(-5,5),transform.position.y,transform.position.z + Random.Range(-5,5)),Quaternion.identity);
+				Instantiate(coin, new Vector3(transform.position.x + Random.Range(-2,2),transform.position.y,transform.position.z + Random.Range(-2,2)),Quaternion.identity);
 			}
 			Destroy (gameObject);
 			Destroy(this);
